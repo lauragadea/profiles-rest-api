301e9d6a10533837fb5abccef177a192c6432481
lauragadea <lauragadea@gmail.com>
2022-04-12 13:22:52 -0300

    adding admin settings

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from profiles_api import models
+
+
+# this tells the django admin to register our UserProfile model with the admin site
+# so it is accesible by the admin interface
+admin.site.register(models.UserProfile)
